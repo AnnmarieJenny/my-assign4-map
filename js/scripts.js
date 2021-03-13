9150899ab2159fcdcdf1dcfc5bfff6fd17e1ce0b
Annmarie Rodriguez <55721341+AnnmarieJenny@users.noreply.github.com>
2021-03-12 22:52:08 -0500

    attempt to add multiple properties to popup
    created VacantLots function to specify which data to pull the properties from

--- js/scripts.js
+++ js/scripts.js
@@ -19,12 +19,13 @@ new MapboxGeocoder({
 var nav = new mapboxgl.NavigationControl();
 map.addControl(nav, 'top-left');
 
-// add data for layer (vacant lots)
-map.on('load', function(){ // was 'style.load' before
+// add data for layer
+// add funciton (VacantLots) to connect data to properties below
+map.on('load', function(VacantLots){ // was 'style.load' before
   // add a geojson source
 map.addSource('nyc_vacant_lots_pluto', {
       type: 'geojson',
-      data: '/data/pluto-map-qgisedited.geojson'
+      data: 'data/pluto-map-qgisedited.geojson'
   });
 
 //specify info for layer to be projected from data
@@ -40,82 +41,30 @@ map.addLayer({
     }
   });
 
+// use function (VacantLots) from above to create a var that pulls from that data
+var myHTML = `
+    <div><b>Address:</b>${VacantLots.Address}</div>
+    <div><b>Owner Name:</b>${VacantLots.OwnerName}</div>
+    <div><b>Assessment Value of Land:</b>${VacantLots.AssessLand}</div>
+    <div><b>BBL:</b>${VacantLots.bbl}</div>`
+
+// create pop up with multiple properties listed above
 map.on('click', 'pluto-vacant-fill', function (e) {
   new mapboxgl.Popup()
-    .setLngLat(e.coordinates)
-    .setHTML(e.features[0].properties.OwnerName)
+    .setLngLat(e.lngLat)
+    .setHTML(e.features[0].properties.myHTML)
     .addTo(map);
   });
 
+// turn mouse into pointer when it hovers over vacant lot geos
 map.on('mousemove', 'pluto-vacant-fill', (e) => {
   map.getCanvas().style.cursor = 'pointer';
 
+// turn pointer off when it hovers away from vacant lot geos 
 map.on('mouseleave', 'pluto-vacant-fill', (e) => {
   map.getCanvas().style.cursor = '';
 })
 
-//var VacantID = null; // << what did this do again?
-
-new mapboxgl.Popup()
-  .setLngLat(coordinates)
 });
-
-  // add a layer to style and display the addSource
-
-  /*map.on('mousemove', function(e)) {
-    //query for the features under the mouse, but only for vacant lots layer
-    var features = map.queryRenderedFeatures(e.point,){
-      layers:['nyc_vacant_lots_pluto'],
-    }
-  });
-    console.log(features)
-  })
-
-  // Create a popup, but don't add it to the map yet
-  //source: mapbox.com/mapbox-gl-js/example/popup-on-hover/
-  var popup = new mapboxgl.Popup({
-    closeButton: false,
-    closeOnClick: false,
-  });*/
-
-/*map.on('style.load', function(){
-  // add a geojson source
-    map.addSource('nyc_vacant_lots_join', {
-      type: 'geojson',
-      data: '/data/vacant_pluto_opendata.geojson'
-  });
-
-  // add a layer to style and display the addSource
-  map.addLayer({
-    'id': 'join-vacant-fill',
-    'type': 'fill',
-    'source': 'nyc_vacant_lots_join',
-    'layout': {},
-    'paint': {
-      'fill-color': '#088',
-      'fill-opacity': 0.8
-    }
-  });
-})*/
-
-/*map.on('style.load', function(){
-  // add a geojson source
-    map.addSource('nyc_vacant_lots_points', {
-      type: 'geojson',
-      data: '/data/vacant_join_point.geojson'
-  });
-
-  // add a layer to style and display the addSource
-  map.addLayer({
-    'id': 'points-vacant-fill',
-    'type': 'fill',
-    'source': 'nyc_vacant_lots_points',
-    'layout': {},
-    'paint': {
-      'fill-color': '#088',
-      'fill-opacity': 0.8
-    }
-  });
-})*/
 
 })
